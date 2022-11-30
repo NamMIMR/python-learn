e74f5787bef1163493c7e2c16d19dfe521b6d351
nam-work <zhounan@mimr.com.cn>
2022-11-30 17:04:30 +0800

    Update dogcalc.py

--- learn to code with python/ch2/dogcalc.py
+++ learn to code with python/ch2/dogcalc.py
@@ -1,3 +1,4 @@
 dog_name = input("What is your dog's name?: ")
 dog_age = input("What is your dog's age?: ")
-dog_age = int(dog_age)
+human_age = int(dog_age) * 7
+print('Your dog ' + dog_name + 'is ' + str(human_age) + ' years old in human years')
