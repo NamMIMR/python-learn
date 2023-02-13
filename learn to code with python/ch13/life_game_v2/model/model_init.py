30db4609f308599aca9e7b5e7e5e94dffcccf026
Nam-desktop <3116004324@mail2.gdut.edu.cn>
2023-02-13 20:59:34 +0800

    model init

--- learn to code with python/ch13/life_game_v2/model/model_init.py
+++ learn to code with python/ch13/life_game_v2/model/model_init.py
@@ -1,14 +1,14 @@
 from typing import List
 from patterns import *
-# import sys
-# sys.path.append('../..')
-# import life_game_v2
-# from life_game_v2.model import patterns
 
 
-def setup_model(height: int, width: int, pattern: str = 'default'):
-    pass
-    return None
+def setup_model(height: int, width: int, pattern: str = 'default') -> GridModel:
+    global model
+    if pattern == 'default':
+        model = GridModel(height, width)
+    elif pattern == 'random':
+        model = RandomGridModel(height, width)
+    return model
 
 if __name__ == "__main__":
     model = setup_model(100, 100)
--- learn to code with python/ch13/life_game_v2/model/patterns.py
+++ learn to code with python/ch13/life_game_v2/model/patterns.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-
+from random import randint
 
 class GridModel:
     def __init__(self, height: int, width: int):
@@ -9,7 +9,50 @@ class GridModel:
     def setup_model(self):
         self.start_model = [[0] * self.width for i in range(0, self.height)]
         self.next_model = [[0] * self.width for i in range(0, self.height)]
-        return self.start_model, self.next_model
+        # return self.start_model, self.next_model
+
+    def count_neighbors(self, row: int, col: int) -> int:
+        count = 0
+
+        if row-1 >= 0:
+            count = count + self.start_model[row-1][col]
+        if (row-1 >= 0) and (col-1 >=  0):
+            count = count + self.start_model[row-1][col-1]
+        if (row-1 >= 0) and (col+1 < self.width):
+            count = count + self.start_model[row-1][col+1]
+        if col-1 >= 0:
+            count = count + self.start_model[row][col-1]
+        if col+1 < self.width:
+            count = count + self.start_model[row][col+1]
+        if row+1 < self.height:
+            count = count + self.start_model[row+1][col]
+        if (row+1 < self.height) and (col-1 >=0):
+            count = count + self.start_model[row+1][col-1]
+        if (row+1 < self.height) and (col+1 < self.width):
+            count = count + self.start_model[row+1][col+1]
+
+        return count
 
     def gen_next(self):
-        pass
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                cell = 0
+                neighbors = self.count_neighbors(i, j)
+
+                if self.start_model[i][j] == 0 & neighbors == 3:
+                    cell = 1
+                elif self.start_model[i][j] == 1 & (neighbors==2 or neighbors==3):
+                    cell = 1
+                self.next_model[i][j] = cell
+        self.start_model, self.next_model = self.next_model, self.start_model
+
+
+class RandomGridModel(GridModel):
+    def __init__(self, height: int, width: int):
+        super().__init__(height, width)
+
+    def setup_model(self):
+        super().setup_model()
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                self.start_model[i][j] = randint(0, 1)
