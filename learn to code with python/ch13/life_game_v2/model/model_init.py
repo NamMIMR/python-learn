9afb24b5da55461667712d067c062399eef00a81
Nam-desktop <3116004324@mail2.gdut.edu.cn>
2023-02-13 20:32:15 +0800

    update

--- learn to code with python/ch13/life_game_v2/model/model_init.py
+++ learn to code with python/ch13/life_game_v2/model/model_init.py
@@ -1,13 +1,14 @@
 from typing import List
-import sys
-sys.path.append('../..')
-import life_game_v2
-from life_game_v2.model import patterns
+from patterns import *
+# import sys
+# sys.path.append('../..')
+# import life_game_v2
+# from life_game_v2.model import patterns
 
 
-def setup_model(height: int, width: int, pattern: str = 'default') -> List[List[int]]:
+def setup_model(height: int, width: int, pattern: str = 'default') -> List[List[int]] or None:
     pass
-
+    return None
 
 if __name__ == "__main__":
     model = setup_model(100, 100)
--- learn to code with python/ch13/life_game_v2/model/patterns.py
+++ learn to code with python/ch13/life_game_v2/model/patterns.py
@@ -1,19 +1,15 @@
-from typing import List
 
 
-def class GridModel:
+class GridModel:
     def __init__(self, height: int, width: int) -> None:
         self.height = height
         self.width = width
-    
-    def start_model(self) -> List[List[int]]:
+
+    def start_model(self) -> None:
+        self.start_model = [[0] * self.width for i in range(0, self.height)]
+
+    def next_model(self) -> None:
         pass
 
-    def next_model(self) -> List[List[int]]:
+    def gen_next(self):
         pass
-
-    def setup_model(self) -> List[List[int]]:
-        grid_model = start_model()
-        next_grid_model = next_model()
-        global grid_model, next_grid_model
-        return grid_model, next_grid_model
