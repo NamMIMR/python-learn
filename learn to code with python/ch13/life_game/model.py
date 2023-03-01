2293b31668d2e915e4e9f7a641507edaeec840c9
Nam-desktop <3116004324@mail2.gdut.edu.cn>
2023-03-01 23:11:17 +0800

    view

--- learn to code with python/ch13/life_game/model.py
+++ learn to code with python/ch13/life_game/model.py
@@ -1,16 +1,27 @@
-
+from typing import List
+import random
 
 
 height = 100
 width = 100
 
+def randomize(grid, width, height):
+    for i in range(0, height):
+        for j in range(0, width):
+            grid[i][j] = random.randint(0, 1)
+
+
 grid_model = [0] * height
+next_grid_model = [0] * height
 
 for i in range(height):
     grid_model[i] = [0] * width
+    next_grid_model[i] = [0] * width
+
+randomize(grid_model, width, height)
 
 def next_gen():
-    global grid_model
+    global grid_model, next_grid_model
 
     for i in range(0, height):
         for j in range(0, width):
@@ -24,6 +35,12 @@ def next_gen():
             elif grid_model[i][j] == 1:
                 if count==2 or count==3:
                     cell = 1
+            next_grid_model[i][j] = cell
+            print("new value is:", next_grid_model[i][j])
+
+    grid_model, next_grid_model = next_grid_model, grid_model
+
+    return grid_model, next_grid_model
 
 
 def count_neighbors(grid, row, col):
